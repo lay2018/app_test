7db13b4e5c56655b289339cbe4b030a94c7b0b86
501-18-PC\501-18 <deltlay@gmail.com>
2018-01-16 11:40:19 +0900

    연산 예제.

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -1,17 +1,27 @@
 public class Exam {
     public static void main(String[] args){
 
-        /*int num1;
-        num1 = 10;
+            int n1=7;
+            int n2=3;
 
-        int num2=20;
-        int num3=num1+num2;
-        System.out.println(num1+"+"+num2+"="+num3);*/
+            int result=n1+n2;
+            System.out.println("덧셈 결과 : "+result);
 
-        char tmp = 'a';
-        double pi = 3.14;
+            result= n1-n2;
+            System.out.println("뺄셈 결과 : "+result);
+            System.out.println("곱셈 결과 : "+n1*n2);
+            System.out.println("나눗셈 결과 : "+n1/n2);
+            System.out.println("나머지 결과 : "+n1%n2);
+            System.out.println();
 
-        System.out.printf("%c=%f", tmp, pi);
+            int num1=30, num2=60, num3=90;
+            int sum = num1 + num2 + num3;
+            double average=sum/3;
+
+            System.out.println(sum);
+            System.out.println(average);
+
+
 
 
 
