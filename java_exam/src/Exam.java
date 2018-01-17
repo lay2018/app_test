b69e66f3d52722642c29cb1524bb0e25d4777595
501-18-PC\501-18 <deltlay@gmail.com>
2018-01-17 14:18:38 +0900

    if~else문 연습

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -1,29 +1,21 @@
 public class Exam {
     public static void main(String[] args){
-
-            int n1=7;
-            int n2=3;
-
-            int result=n1+n2;
-            System.out.println("덧셈 결과 : "+result);
-
-            result= n1-n2;
-            System.out.println("뺄셈 결과 : "+result);
-            System.out.println("곱셈 결과 : "+n1*n2);
-            System.out.println("나눗셈 결과 : "+n1/n2);
-            System.out.println("나머지 결과 : "+n1%n2);
-            System.out.println();
-
-            int num1=30, num2=60, num3=90;
-            int sum = num1 + num2 + num3;
-            double average=sum/3;
-
-            System.out.println(sum);
-            System.out.println(average);
-
-
-
-
+        int score=98;
+        if(score>=90){
+            System.out.println("성적은 수 입니다.");
+        }
+        else if(score>=80) {
+            System.out.println("성적은 우 입니다.");
+        }
+        else if(score>=70) {
+            System.out.println("성적은 미 입니다.");
+        }
+        else if(score>=60) {
+            System.out.println("성적은 양 입니다.");
+        }
+        else{
+            System.out.println("성적은 가 입니다.");
+        }
 
     }
 }
