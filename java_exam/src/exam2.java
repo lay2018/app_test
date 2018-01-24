a82de53c250c98002881ed5c4d8955646f57a7b1
501-18-PC\501-18 <deltlay@gmail.com>
2018-01-24 11:40:58 +0900

    기초문제 _ 선택제어 3

--- java_exam/src/exam2.java
+++ java_exam/src/exam2.java
@@ -4,19 +4,19 @@ public class exam2 {
     public static void main(String[] args){
 
 
-        System.out.print("키와 몸무게를 입력하세요.>");
+        System.out.print("나이를 입력하세요.>");
 
         Scanner scanner=new Scanner(System.in);
         int num1=scanner.nextInt();
-        int num2=scanner.nextInt();
 
-        if(num2+100-num1>0) {
-            System.out.println(num2+100-num1);
-            System.out.print("obesity");
+
+        if(num1>20) {
+
+            System.out.print("adult");
         }
 
         else
-            System.out.print(num2+100-num1);
+            System.out.print(20-num1+"years later");
 
 
 
