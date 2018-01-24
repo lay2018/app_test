9ae6494ed91e08d4ae8e705163b73647b7770e54
501-18-PC\501-18 <deltlay@gmail.com>
2018-01-24 13:04:28 +0900

    break문.

--- java_exam/src/exam2.java
+++ java_exam/src/exam2.java
@@ -4,21 +4,14 @@ public class exam2 {
     public static void main(String[] args){
 
 
-        System.out.print("나이를 입력하세요.>");
-
-        Scanner scanner=new Scanner(System.in);
-        int num1=scanner.nextInt();
-
-
-        if(num1>20) {
-
-            System.out.print("adult");
+    int num =2;
+    while(num < 10) {
+        if(num%5==0) {
+            break;
         }
-
-        else
-            System.out.print(20-num1+"years later");
-
-
+        System.out.println(num);
+        num++;
+    }
 
     }
 
