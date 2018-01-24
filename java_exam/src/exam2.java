867a4fb8228821d2ccba5fac55132c9b8606fd57
501-18-PC\501-18 <deltlay@gmail.com>
2018-01-24 13:14:40 +0900

    최소공배수구하기

--- java_exam/src/exam2.java
+++ java_exam/src/exam2.java
@@ -4,14 +4,16 @@ public class exam2 {
     public static void main(String[] args){
 
 
-    int num =2;
-    while(num < 10) {
-        if(num%5==0) {
+    int num =1;
+    //6과 14의 최소공배수를 구하는 방법.
+    while(true){
+        if (num%6==0 && num%14==0) {
             break;
         }
-        System.out.println(num);
         num++;
     }
+
+    System.out.print(num);
 
     }
 
