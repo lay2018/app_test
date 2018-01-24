94a2360971b79ca462254ad8f49ca1c1adc278ed
501-18-PC\501-18 <deltlay@gmail.com>
2018-01-24 11:37:36 +0900

    문제풀이 - 선택제어 추가2

--- java_exam/src/exam2.java
+++ java_exam/src/exam2.java
@@ -3,33 +3,20 @@ import java.util.Scanner;
 public class exam2 {
     public static void main(String[] args){
 
-//        System.out.print("자연수를 입력하시오.>");
-//        Scanner scanner = new Scanner(System.in);
-//                int num =scanner.nextInt();
-//
-//            for(int i=1;i<=10; i++) {
-//
-//                System.out.print(num*i+" ");
-//        System.out.print("자연수 두 개를 입력하시오.>");
-//
-//        int num1;
-//        int num2;
-//        Scanner scanner = new Scanner(System.in);
-//                 num1 = scanner.nextInt();
-//                 num2 = scanner.nextInt();
-//
-        System.out.print("숫자를 입력하세요.>");
+
+        System.out.print("키와 몸무게를 입력하세요.>");
 
         Scanner scanner=new Scanner(System.in);
-        int num=scanner.nextInt();
+        int num1=scanner.nextInt();
+        int num2=scanner.nextInt();
 
-        if(num<0) {
-            System.out.println(num);
-            System.out.print("minus");
+        if(num2+100-num1>0) {
+            System.out.println(num2+100-num1);
+            System.out.print("obesity");
         }
 
         else
-            System.out.print(num);
+            System.out.print(num2+100-num1);
 
 
 
