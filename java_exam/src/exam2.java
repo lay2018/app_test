4717d046a93e52f1231aa75ff62066125ee75ab5
501-18-PC\501-18 <deltlay@gmail.com>
2018-01-30 12:49:48 +0900

    매개변수 미니계산기

--- java_exam/src/exam2.java
+++ java_exam/src/exam2.java
@@ -5,20 +5,87 @@ public class exam2 {
 
         double num1;
         double num2;
-        System.out.print("계산이 필요한 숫자를 2개 입력하시오.>");
+        int operator;
+//        double res;
 
-        Scanner scanner = new Scanner(System.in);
-        num1 = scanner.nextDouble();
-        num2 = scanner.nextDouble();
+        while(true) {
+
+            System.out.print("첫 번째 숫자를 입력하세요.>");
+
+            Scanner scanner = new Scanner(System.in);
+            num1 = scanner.nextDouble();
+
+            System.out.println("==============");
+            System.out.println("[1] 더하기");
+            System.out.println("[2] 빼기");
+            System.out.println("[3] 곱하기");
+            System.out.println("[4] 나누기");
+            System.out.println("[5] 나머지");
+            System.out.println("==============");
+            System.out.print("연산자를 선택하세요.>");
+
+            operator = scanner.nextInt();
+
+            System.out.print("두 번째 숫자를 입력하세요.>");
+            num2 = scanner.nextDouble();
 
 
-        System.out.println(adder(num1,num2));
-        System.out.println(multi(num1,num2));
-        System.out.println(minus(num1,num2));
-        System.out.println(divide(num1,num2));
-        System.out.println(reminder(num1,num2));
+            switch (operator) {
+                case 1:
+                    System.out.println("덧셈:"+num1+"+"+num2+"="+adder(num1,num2));
 
-        System.out.println("계산 종료");
+//                res=adder(num1,num2);
+//                System.out.println(res+2);
+                    break;
+                case 2:
+                    System.out.println("뺄셈:"+num1+"-"+num2+"="+minus(num1,num2));
+                    if(minus(num1,num2)<0){
+                        System.out.println("결과값이 "+minus(num1,num2)+" 입니다.");
+                        System.out.println("양수로 변환한 값은"+(-minus(num1,num2))+"입니다.");
+                    }
+                    break;
+                case 3:
+                    System.out.println("곱셈:"+num1+"*"+num2+"="+multi(num1,num2));
+                    break;
+                case 4:
+                    if(num2==0){
+                        System.out.println("나눗셈을 할 수 없습니다.");
+                    }
+                    else {
+                        System.out.println("나눗셈:" + num1 + "/" + num2 + "=" + divide(num1, num2));
+                    }
+                    break;
+                case 5:
+                    System.out.println("나머지:"+num1+"%"+num2+"="+reminder(num1,num2));
+                    break;
+//                case 6:
+//                    System.out.println("num1의 절대값:"+absolute(num1));
+//                    System.out.println("num2의 절대값:"+absolute(num2));
+//                    break;
+                default:
+                    System.out.println("잘못 선택하셨습니다. 다시 실행하세요.");
+            }
+
+            System.out.println("계산 종료");
+//
+
+        }
+
+
+
+
+
+
+
+
+
+//        System.out.println(adder(num1,num2));
+//        System.out.println(multi(num1,num2));
+//        System.out.println(minus(num1,num2));
+//        System.out.println(divide(num1,num2));
+//        System.out.println(reminder(num1,num2));
+
+
 
     }
 
@@ -53,6 +120,16 @@ public class exam2 {
         return result;
     }
 
+//    public static double absolute(double num1, double num2){
+//        double result;
+//        if (num1<=0){
+//
+//            return result;
+////    }
+//
+//            num1 *= -1;
+//        }
+//
     }
 
 
