9b9e6bd24c9b30f3d18953fb024151cbe3850350
501-18-PC\501-18 <deltlay@gmail.com>
2018-01-30 10:45:04 +0900

    매개변수 연습

--- java_exam/src/exam2.java
+++ java_exam/src/exam2.java
@@ -3,26 +3,55 @@ import java.util.Scanner;
 public class exam2 {
     public static void main(String[] args) {
 
-        boolean scope = true;
+        double num1;
+        double num2;
+        System.out.print("계산이 필요한 숫자를 2개 입력하시오.>");
 
-        if (scope) {
-            int num = 1;
-            num++;
-            System.out.println(num);
+        Scanner scanner = new Scanner(System.in);
+        num1 = scanner.nextDouble();
+        num2 = scanner.nextDouble();
 
-        } else {
-            int num = 2;
-            System.out.println(num);
-        }
 
-        simple();
+        System.out.println(adder(num1,num2));
+        System.out.println(multi(num1,num2));
+        System.out.println(minus(num1,num2));
+        System.out.println(divide(num1,num2));
+        System.out.println(reminder(num1,num2));
+
+        System.out.println("계산 종료");
+
     }
 
-    public static void simple()
-        {
-            int num=3;
-            System.out.println(num);
-        }
+
+    public static double adder(double num1, double num2) {
+        double result;
+        result = num1 + num2;
+        return result;
+    }
+
+    public static double multi(double num1, double num2){
+        double result;
+        result = num1 * num2;
+        return result;
+    }
+
+    public static double minus(double num1, double num2){
+        double result;
+        result = num1 - num2;
+        return result;
+    }
+
+    public static double divide(double num1, double num2){
+        double result;
+        result = num1 / num2;
+        return result;
+    }
+
+    public static double reminder(double num1, double num2){
+        double result;
+        result = num1 % num2;
+        return result;
+    }
 
     }
 
